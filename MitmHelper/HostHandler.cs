0d3f364b856d253aec2952dd53a353953cee0cbb
erikmoch <erikrozamoch@gmail.com>
2023-02-09 01:36:09 -0300

    Adicionar arquivos de projeto.

---
+++ MitmHelper/HostHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+
+namespace MitmHelper
+{
+    public static class HostHandler
+    {
+        private static readonly string HostsFilePath = Path.Combine(Environment.SystemDirectory, "drivers\\etc\\hosts");
+
+        static HostHandler()
+        {
+            TryRemoveRedirects();
+        }
+
+        public static bool TryAddRedirect(string original, string redirect)
+        {
+            try
+            {
+                string match = $"{original} {redirect}";
+                if (!File.ReadAllLines(HostsFilePath).Any(l => l.Contains(match)))
+                    File.AppendAllText(HostsFilePath, $"\r\n{original} {redirect} #MitmHelper");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding redirect: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static bool TryRemoveRedirects()
+        {
+            try
+            {
+                File.WriteAllLines(HostsFilePath, File.ReadAllLines(HostsFilePath).Where(l => !l.EndsWith("#MitmHelper") && l != string.Empty));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error removing redirects: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static IPAddress GetIPAddressFromHost(string host)
+        {
+            try
+            {
+                return Dns.GetHostEntry(host).AddressList.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting IP address from host: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ MitmHelper/IpRedirector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MitmHelper
+{
+    internal class IpRedirector
+    {
+        public static void Enable()
+        {
+            RunCommand("netsh", "interface", "ip", "set", "dns", "1", "dhcp");
+        }
+
+        public static void AddAddress(string ip)
+        {
+            RunCommand("netsh", "interface", "ip", "add", "address", "\"Loopback\"", ip, "255.255.255.255");
+        }
+
+        public static void RemoveAddress(string ip)
+        {
+            RunCommand("netsh", "interface", "ip", "delete", "address", "\"Loopback\"", ip);
+        }
+
+        private static void RunCommand(params string[] command)
+        {
+            try
+            {
+                ProcessStartInfo start = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    Arguments = string.Join(" ", command)
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = start;
+                    process.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing command: {ex.Message}");
+            }
+        }
+    }
+}
